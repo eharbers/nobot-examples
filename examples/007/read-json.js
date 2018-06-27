a8bc94fe64dc08e84986847d480e5a47bcfdb059
eharbers <erik.harbers64@gmail.com>
2018-06-27 16:29:46 +0200

    tweede commit

---
+++ examples/007/read-json.js
@@ -0,0 +1,4 @@
+const {projectId, startDate, endDate } = require('./data/example-config');
+console.log(`The ID of the project is: ${projectId}`);
+console.log(`The start date of the projcet is: ${startDate}`);
+console.log(`The end date of the project is: ${endDate}`);
