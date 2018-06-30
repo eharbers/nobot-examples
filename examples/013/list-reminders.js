eb37eb2749448e9f92b50e58a71830649bc04744
eharbers <erik.harbers64@gmail.com>
2018-07-01 00:08:40 +0200

    013 test

--- examples/013/list-reminders.js
+++ examples/013/list-reminders.js
@@ -2,3 +2,11 @@ require('colors');
 const fs = require('fs');
 const readLineSync = require('readline-sync');
 const { JSON_WHITESPACE, NO_CHOICE_MADE } = require('./constants');
+const {reminders} = require('./.reminders');
+if (reminders.length === 0) {
+    console.log('No reminders!'.green);
+    process.exit(0);
+}
+console.log(`you removed '${reminders[index]}'`.red);
+reminders.splice(index, 1);
+fs.writeFileSync(`${__dirname}/.reminders.json`, JSON.stringify({reminders}, null, JSON_WHITESPACE));
